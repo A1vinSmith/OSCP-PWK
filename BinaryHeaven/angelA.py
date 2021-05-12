f61357dd8e02ee1600a8667844b20b9c22239aef
Alvin Smith <alvin.smith@totaralearning.com>
2021-05-13 11:46:27 +1200

    Thank you https://stackoverflow.com/questions/209513/convert-hex-string-to-int-in-python

--- BinaryHeaven/angelA.py
+++ BinaryHeaven/angelA.py
@@ -6,3 +6,13 @@ for char in hex_values:
 	username += chr((int(char, 0) - 8) ^ 4)
 
 print(username)
+
+
+# Alternatie with specify the base explicitly
+hex_values2 = ['6b', '79', '6d', '7e', '68', '75', '6d', '72']
+
+username2 = ""
+for char in hex_values2:
+	username2 += chr((int(char, 16) - 8) ^ 4)
+
+print(username2)
