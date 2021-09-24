9ea2851e5bcda737b12062b4092f447778c6ab40
Alvin Smith <alvinsmith.nz@gmail.com>
2021-09-25 11:03:45 +1200

    Update 0-100.sh

--- Script/Bash/0-100.sh
+++ Script/Bash/0-100.sh
@@ -1,5 +1,7 @@
 # for i in `seq 1 100`; do echo $i; done
 for i in `seq 0 100`; do echo $i >> num.txt; done
 
+for i in {0..255}; do echo $i >> num2.txt; done
+
 # Alternatives
 # /usr/share/wordlists/seclists/Fuzzing/
